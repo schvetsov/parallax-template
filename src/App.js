8fdefa6528e9fdae0481dcf571db469dc6ed78a8
Stan Chvetsov <sac08xyz@gmail.com>
2019-08-10 19:14:57 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import headshot from './headshot.jpg';
 import './App.css';
 
-function App() {
+function App(props) {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={"root"} >
+
+      <Navbar bg="primary" variant="dark">
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+        <Form inline>
+          <Form.Control type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-light">Search</Button>
+        </Form>
+      </Navbar>
+
+      <div className={"title"}>
+        Beach Bum Beauty
+      </div>
+
+      <div className={"headshot"}>
+        <img src={headshot} alt="image1" style={{width: 200}} />
+      </div>
+
     </div>
   );
 }
-
 export default App;
